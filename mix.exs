fc9f045aa9b2a376b6e06f67f4ea29d34f65877b
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2025-05-04 15:28:34 -0500

    feat: bump version to 0.2.0-dev

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsSsoConfigGenerator.MixProject do
   def project do
     [
       app: :aws_sso_config_generator,
-      version: "0.1.0",
+      version: "0.2.0-dev",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
