ad7d2b818b81eb4c9e6a49acd6dc9562b2da7c07
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2025-08-20 23:58:29 -0500

    chore: Re-work github workflow to use mise

--- version-info.exs
+++ version-info.exs
@@ -1,24 +1,30 @@
+require Logger
+
 version = Regex.run(~r/version: "(.*)"/, File.read!("mix.exs"), capture: :all_but_first) |> List.first()
-IO.puts("version #{version}")
+Logger.info("version #{version}")
 
 File.cd!("burrito_out")
 
 for file <- File.ls!() do
   destination = String.replace(file, "_", "-")
-  IO.puts("mv #{file} #{destination}")
+  Logger.info("mv #{file} #{destination}")
   File.rename!(file, destination)
 end
 
+Logger.info("creating aws-sso-config-generator-checksums.txt")
 {"", 0} = System.shell("shasum -a 256 * > aws-sso-config-generator-checksums.txt")
 
 File.cd!("..")
 
 if System.get_env("CI") do
   if String.ends_with?(version, "-dev") do
+    Logger.info("creating github prerelease")
     System.shell("gh release create #{version} --generate-notes -p")
   else
+    Logger.info("creating github release")
     System.shell("gh release create #{version} --generate-notes")
   end
 
+  Logger.info("uploading artifacts")
   System.shell("gh release upload #{version} ./burrito_out/* -R djgoku/aws-sso-config-generator")
 end
