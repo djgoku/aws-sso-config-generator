9b613e4676568ef9bf67d9cad3954b9a0829d474
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2025-08-21 00:08:44 -0500

    chore: bump to 0.3.2
    This switch is just to switch mostly from devbox to mise. This allows
    less dependencies in nix land since mise is pretty good at managing
    binaries for programming languages or cli tools.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsSsoConfigGenerator.MixProject do
   def project do
     [
       app: :aws_sso_config_generator,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
