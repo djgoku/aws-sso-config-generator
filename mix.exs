d004b06621080de77aebbfe59be312b606069170
Jonathan Carroll Otsuka <105506+djgoku@users.noreply.github.com>
2025-10-16 20:56:03 -0500

    bump: Bump aws-sso-config-generator to version 0.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsSsoConfigGenerator.MixProject do
   def project do
     [
       app: :aws_sso_config_generator,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
