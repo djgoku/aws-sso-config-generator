8afded9b62a6fe18dece542e57c89ead56c9ad54
Jonathan Carroll Otsuka <dj_goku@Mac-Studio.local>
2026-03-19 22:34:40 -0500

    bump: version to 0.4.2-alpha for SSO pagination fix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AwsSsoConfigGenerator.MixProject do
   def project do
     [
       app: :aws_sso_config_generator,
-      version: "0.4.1",
+      version: "0.4.2-alpha",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
